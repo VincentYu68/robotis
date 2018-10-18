cb0668c6ae9b48604e56b1ffe66e8549a9134584
robotis <robotis@robotis.(none)>
2018-10-18 09:42:42 +0900

    fix bug not entirely fixed

--- Linux/project/experiments/arm_lift.cpp
+++ Linux/project/experiments/arm_lift.cpp
@@ -8,6 +8,14 @@
 
 using namespace Robot;
 
+double angle2rad(double angle) {
+    return angle / 180 * 3.141592653589;
+}
+
+double rad2angle(double rad) {
+    return rad / 3.141592653589 * 180;
+}
+
 int main()
 {
 	printf( "\n===== Test DARwIn Armlifting =====\n\n");
@@ -30,11 +38,11 @@ int main()
         0.01382301};
     int init_pose_value[20];
     for (int i = 0; i < 20; i++) {
-        init_pose_value[i] = MX28::Angle2Value(init_pose_angle[i]);
+        init_pose_value[i] = MX28::Angle2Value(rad2angle(init_pose_angle[i]));
     }
 
-    fprintf(init_pose_value);
-
+    for (int i = 0; i < 20; i++)
+        printf("%f,%d\n",init_pose_angle[i], init_pose_value[i]);
 	int param[JointData::NUMBER_OF_JOINTS * MX28::PARAM_BYTES];
     int n = 0;
     int joint_num = 0;
@@ -53,12 +61,14 @@ int main()
     }
 
     if(joint_num > 0)
-        m_CM730->SyncWrite(MX28::P_D_GAIN, MX28::PARAM_BYTES, joint_num, param);
+        cm730.SyncWrite(MX28::P_D_GAIN, MX28::PARAM_BYTES, joint_num, param);
     ///////////////////////////////////////////////////////////////////////
 
 
     std::ofstream fileStream;
-    fileStream.open('data.txt', std::ios::out)
+    char fname[32] = {0,};
+    sprintf(fname, "data.txt");    
+    fileStream.open(fname, std::ios::out);
 
 	int value;
 
@@ -81,18 +91,18 @@ int main()
 		if(cm730.ReadWord(JointData::ID_L_SHOULDER_PITCH, MX28::P_PRESENT_POSITION_L, &value, 0) == CM730::SUCCESS)
 		{
 		    cur_shoulder_pitch_angle = MX28::Value2Angle(value);
-		    goal = (num_steps * 1.0 / interp_steps) * goal_shoulder_pitch + (interp_steps - num_steps) * 1.0 / interp_steps * init_shoulder_pitch;
+		    int goal = (num_steps * 1.0 / interp_steps) * goal_shoulder_pitch + (interp_steps - num_steps) * 1.0 / interp_steps * init_shoulder_pitch;
 			cm730.WriteWord(JointData::ID_L_SHOULDER_PITCH, MX28::P_GOAL_POSITION_L, goal, 0);
 		}
 		else
 			printf("Can't read angle from l shoulder pitch");
 			return 1;
 
-		if(cm730.ReadWord(JointData::ID_L_SHOULDER_ROW, MX28::P_PRESENT_POSITION_L, &value, 0) == CM730::SUCCESS)
+		if(cm730.ReadWord(JointData::ID_L_SHOULDER_ROLL, MX28::P_PRESENT_POSITION_L, &value, 0) == CM730::SUCCESS)
 		{
 		    cur_shoulder_row_angle = MX28::Value2Angle(value);
-		    goal = (num_steps * 1.0 / interp_steps) * goal_shoulder_row + (interp_steps - num_steps) * 1.0 / interp_steps * init_shoulder_row;
-			cm730.WriteWord(JointData::ID_L_SHOULDER_ROW, MX28::P_GOAL_POSITION_L, goal, 0);
+		    int goal = (num_steps * 1.0 / interp_steps) * goal_shoulder_row + (interp_steps - num_steps) * 1.0 / interp_steps * init_shoulder_row;
+			cm730.WriteWord(JointData::ID_L_SHOULDER_ROLL, MX28::P_GOAL_POSITION_L, goal, 0);
 		}
 		else
 			printf("Can't read angle from l shoulder row");
@@ -101,21 +111,21 @@ int main()
 	    if(cm730.ReadWord(JointData::ID_L_ELBOW, MX28::P_PRESENT_POSITION_L, &value, 0) == CM730::SUCCESS)
 		{
 		    cur_elbow_angle = MX28::Value2Angle(value);
-		    goal = (num_steps * 1.0 / interp_steps) * goal_elbow + (interp_steps - num_steps) * 1.0 / interp_steps * init_elbow;
+		    int goal = (num_steps * 1.0 / interp_steps) * goal_elbow + (interp_steps - num_steps) * 1.0 / interp_steps * init_elbow;
 			cm730.WriteWord(JointData::ID_L_ELBOW, MX28::P_GOAL_POSITION_L, goal, 0);
 		}
 		else
 			printf("Can't read angle from l elbow");
 			return 1;
 
-		fileStream << cur_shoulder_pitch_angle << " " << cur_shoulder_row_angle << " " << cur_elbow_angle << std::endl;
+		fileStream << angle2rad(cur_shoulder_pitch_angle) << " " << angle2rad(cur_shoulder_row_angle) << " " << angle2rad(cur_elbow_angle) << std::endl;
 
 		num_steps ++;
 
 		usleep(20000);
 	}
 
-	fileStream.close()
+	fileStream.close();
 
 	return 0;
 }
