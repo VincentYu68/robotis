8825ee582edd3226be94fa27c4e39d35b9f0a34e
Wenhao Yu <wenhaoyu@Wenhaos-MacBook-Pro.local>
2018-10-17 21:23:59 -0400

    add missing semicolon

--- Linux/project/experiments/arm_lift.cpp
+++ Linux/project/experiments/arm_lift.cpp
@@ -122,7 +122,7 @@ int main()
 		{
 		    cur_shoulder_pitch_angle = MX28::Value2Angle(value);
 		    int goal = (num_steps * 1.0 / interp_steps) * goal_shoulder_pitch + (interp_steps - num_steps) * 1.0 / interp_steps * init_shoulder_pitch;
-		    printf("Goal of pitch %d \n", goal)
+		    printf("Goal of pitch %d \n", goal);
 			cm730.WriteWord(JointData::ID_L_SHOULDER_PITCH, MX28::P_GOAL_POSITION_L, goal, 0);
 		}
 		else
@@ -133,7 +133,7 @@ int main()
 		{
 		    cur_shoulder_row_angle = MX28::Value2Angle(value);
 		    int goal = (num_steps * 1.0 / interp_steps) * goal_shoulder_row + (interp_steps - num_steps) * 1.0 / interp_steps * init_shoulder_row;
-		    printf("Goal of roll %d \n", goal)
+		    printf("Goal of roll %d \n", goal);
 			cm730.WriteWord(JointData::ID_L_SHOULDER_ROLL, MX28::P_GOAL_POSITION_L, goal, 0);
 		}
 		else
@@ -144,7 +144,7 @@ int main()
 		{
 		    cur_elbow_angle = MX28::Value2Angle(value);
 		    int goal = (num_steps * 1.0 / interp_steps) * goal_elbow + (interp_steps - num_steps) * 1.0 / interp_steps * init_elbow;
-		    printf("Goal of elbow %d \n", goal)
+		    printf("Goal of elbow %d \n", goal);
 			cm730.WriteWord(JointData::ID_L_ELBOW, MX28::P_GOAL_POSITION_L, goal, 0);
 		}
 		else
