87c9e838388ab1ca4adf4d3f58a630996f32bcd1
wyu68 <wyu68@wenahoyu@gatech.edu>
2019-03-06 10:14:26 -0500

    updates

--- Linux/build/LinuxMotionTimer.cpp
+++ Linux/build/LinuxMotionTimer.cpp
@@ -28,7 +28,7 @@ void *LinuxMotionTimer::TimerProc(void *param)
 
     while(!timer->m_FinishTimer)
     {
-        float nt = next_time.tv_sec + next_time.tv_nsec / 1000000000.0;
+        float nt = float(next_time.tv_sec) + float(next_time.tv_nsec) / 1000000000.0;
         printf("nex time %f\n", nt);
         next_time.tv_sec += (next_time.tv_nsec + MotionModule::TIME_UNIT * 1000000) / 1000000000;
         next_time.tv_nsec = (next_time.tv_nsec + MotionModule::TIME_UNIT * 1000000) % 1000000000;
